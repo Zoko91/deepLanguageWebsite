30c71af5f10ef45f299703d337cf0fb8351d4314
Zoko91 <josephbeasse@gmail.com>
2023-03-25 15:44:47 +0100

    Website_online_ready

--- myApp.py
+++ myApp.py
@@ -19,15 +19,6 @@ def index():
     current_page = "home"
     return render_template('home.html', current_page=current_page)
 
-@app.route('/upload', methods=['GET','POST'])
-def upload():
-    file = request.files.get('file')
-    file_path = 'static/temp/' + file.filename
-    if os.path.exists(file_path):
-        os.remove(file_path)
-    # Save the file to the server
-    file.save(file_path)
-    return redirect('/process')
 
 @app.route('/save_audio', methods=['POST'])
 def save_audio():
@@ -39,14 +30,6 @@ def save_audio():
     sound = AudioSegment.from_file(audio_data, format='webm')
     sound.export('static/temp/recording.wav', format='wav')
     return redirect(url_for("process"))
-
-def to_wav():
-    file_name = 'static/temp/recording.mp3'
-    # Load the MP3 file using the from_mp3() method
-    mp3_audio = AudioSegment.from_mp3(file_name)
-    output_name = 'static/temp/recording.wav'
-    # Save the audio as a WAV file using the export() method
-    mp3_audio.export(output_name, format="wav")
 
 
 def preprocessMelCoeff(file_path):
@@ -83,42 +66,6 @@ def predictLibrosa(file_path):
 
     return language, language_probability
 
-def record_audio():
-    CHUNK = 1024
-    FORMAT = pyaudio.paInt16
-    CHANNELS = 1
-    RATE = 32000
-    RECORD_SECONDS = 5
-    WAVE_OUTPUT_FILENAME = "static/temp/recording.wav"
-
-    if os.path.exists(WAVE_OUTPUT_FILENAME):
-        os.remove(WAVE_OUTPUT_FILENAME)
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    print("* recording")
-    frames = []
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-    print("* done recording")
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
-
 
 @app.route('/process')
 def process():
@@ -129,9 +76,3 @@ def process():
     language_probability = round(language_probability * 100, 2)
     return render_template('process.html', current_page=current_page, language=language, probability=language_probability)
 
-# A UTILISER QUAND ON APPUIE SUR LE BOUTON
-@app.route("/record", methods=["GET","POST"])
-def record():
-    # Call the record_audio function here
-    record_audio()
-    return redirect(url_for("process"))
